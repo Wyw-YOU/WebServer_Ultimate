4cd5b89f02af97b489cdf102e8ef670752dba7a6
Wyw-YOU <wyw99977@gmail.com>
2026-06-23 20:16:53 +0800

    重要信息使用配置化文档

---
+++ build/CMakeFiles/server.dir/cmake_clean.cmake
@@ -0,0 +1,36 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/server.dir/src/AsyncLogger.cpp.o"
+  "CMakeFiles/server.dir/src/Log.cpp.o"
+  "CMakeFiles/server.dir/src/Server.cpp.o"
+  "CMakeFiles/server.dir/src/Test/ChannelTest.cpp.o"
+  "CMakeFiles/server.dir/src/Test/EventLoopThreadTest.cpp.o"
+  "CMakeFiles/server.dir/src/Test/ThreadPoolTest.cpp.o"
+  "CMakeFiles/server.dir/src/buffer/Buffer.cpp.o"
+  "CMakeFiles/server.dir/src/db/ConnectionPool.cpp.o"
+  "CMakeFiles/server.dir/src/http/HttpContext.cpp.o"
+  "CMakeFiles/server.dir/src/http/HttpRequest.cpp.o"
+  "CMakeFiles/server.dir/src/http/HttpResponse.cpp.o"
+  "CMakeFiles/server.dir/src/http/MimeType.cpp.o"
+  "CMakeFiles/server.dir/src/http/Router.cpp.o"
+  "CMakeFiles/server.dir/src/main.cpp.o"
+  "CMakeFiles/server.dir/src/net/Acceptor.cpp.o"
+  "CMakeFiles/server.dir/src/net/Channel.cpp.o"
+  "CMakeFiles/server.dir/src/net/Connection.cpp.o"
+  "CMakeFiles/server.dir/src/net/Epoll.cpp.o"
+  "CMakeFiles/server.dir/src/net/EventLoop.cpp.o"
+  "CMakeFiles/server.dir/src/net/EventLoopThread.cpp.o"
+  "CMakeFiles/server.dir/src/net/EventLoopThreadPool.cpp.o"
+  "CMakeFiles/server.dir/src/net/InetAddress.cpp.o"
+  "CMakeFiles/server.dir/src/net/Socket.cpp.o"
+  "CMakeFiles/server.dir/src/thread/ThreadPool.cpp.o"
+  "CMakeFiles/server.dir/src/timer/Timer.cpp.o"
+  "CMakeFiles/server.dir/src/util/FileUtil.cpp.o"
+  "CMakeFiles/server.dir/src/util/GzipUtil.cpp.o"
+  "server"
+  "server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
